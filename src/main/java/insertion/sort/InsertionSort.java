510c70b6618b7f760d91e2715eb576f4efbd3b53
HaTien <havantien95tb@gmail.com>
2018-09-19 10:59:52 +0700

    bài tập sắp xếp mảng theo thứ tự tăng dần

---
+++ src/main/java/insertion/sort/InsertionSort.java
@@ -0,0 +1,45 @@
+package insertion.sort;
+
+import java.util.Scanner;
+
+public class InsertionSort {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Nhập số phần tử của mảng: ");
+        int n = sc.nextInt();
+        // khởi tạo arr
+        int [] arr = new int [n];
+        System.out.print("Nhập các phần tử của mảng: \n");
+        for (int i = 0; i < n; i++) {
+            System.out.printf("a[%d] = ", i);
+            arr[i] = sc.nextInt();
+        }
+        // sắp xếp dãy số theo thứ tự tăng dần
+        sortASC(arr);
+        System.out.println("Dãy số được sắp xếp tăng dần: ");
+        show(arr);
+    }
+
+
+     // sắp xếp mảng số nguyên theo thứ tự tăng dần
+    public static void sortASC(int [] arr) {
+        int temp = arr[0];
+        for (int i = 0 ; i < arr.length - 1; i++) {
+            for (int j = i + 1; j < arr.length; j++) {
+                if (arr[i] > arr[j]) {
+                    temp = arr[j];
+                    arr[j] = arr[i];
+                    arr[i] = temp;
+                }
+            }
+        }
+    }
+
+//      in các phần tử của mảng ra màn hình/
+    public static void show(int [] arr) {
+        for (int i = 0; i < arr.length; i++) {
+            System.out.print(arr[i] + " ");
+        }
+    }
+}
+
